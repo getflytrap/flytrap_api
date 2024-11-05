21881bff8a70f51520dec9bef7e85cca0c02e1b1
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-05 12:47:36 +0100

    feat: added helper methods for the db model that fetches all error and rejection data for a project
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,9 +1,12 @@
 from .db_config import get_db_connection
-from .db_helpers import calculate_total_project_pages
+from .db_helpers import calculate_total_project_pages, fetch_errors_by_project, fetch_rejections_by_project, calculate_total_error_pages
 from .uuid_generator import generate_uuid
 
 __all__ = [
     "get_db_connection",
     "calculate_total_project_pages",
+    "fetch_errors_by_project",
+    "fetch_rejections_by_project",
+    "calculate_total_error_pages",
     "generate_uuid"
 ]
