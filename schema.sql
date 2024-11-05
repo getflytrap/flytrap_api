b6db260a275293be3707d9a9b3ba4f826027c0b4
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-05 11:45:55 +0100

    feat: updated schema with projects, rejections, users, and projects_users tables
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- schema.sql
+++ schema.sql
@@ -1,4 +1,14 @@
 DROP TABLE IF EXISTS error_logs;
+DROP TABLE IF EXISTS rejection_logs;
+DROP TABLE IF EXISTS projects_users;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  pid VARCHAR(36) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE error_logs (
     id SERIAL PRIMARY KEY,
@@ -7,7 +17,33 @@ CREATE TABLE error_logs (
     created_at TIMESTAMPTZ NOT NULL,
     line_number INT,
     col_number INT,
-    project_id INT,
+    project_id INT REFERENCES projects(id) ON DELETE CASCADE,
     stack_trace TEXT,
     handled BOOLEAN NOT NULL,
+    resolved BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE rejection_logs (
+  id SERIAL PRIMARY KEY,
+  value TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  handled BOOLEAN NOT NULL,
+  resolved BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    is_root BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE projects_users (
+  id SERIAL PRIMARY KEY,
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE
 );
