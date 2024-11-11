6d80f0436009656b3c44fdb47c90f6082777be9a
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-11 13:16:23 +0100

    fix: removed is_root info from refresh token and replaced by a database check in refresh_access_token
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/utils/auth/token_manager.py
+++ app/utils/auth/token_manager.py
@@ -2,6 +2,7 @@ import jwt
 import datetime
 from flask import request
 from app.config import SECRET_KEY
+from app.models import get_user_root_info
 
 class TokenManager:
     def create_access_token(self, user_uuid, is_root, expires_in=20):
@@ -15,11 +16,10 @@ class TokenManager:
             algorithm="HS256"
         )
 
-    def create_refresh_token(self, user_uuid, is_root, expires_in=7):
+    def create_refresh_token(self, user_uuid, expires_in=7):
         return jwt.encode(
             {
                 "user_uuid": user_uuid,
-                "is_root": is_root,
                 "exp": datetime.datetime.utcnow() + datetime.timedelta(days=expires_in),
             },
             SECRET_KEY,
@@ -31,16 +31,16 @@ class TokenManager:
 
     def refresh_access_token(self):
         refresh_token = request.cookies.get("refresh_token")
-        print("refresh token: ", refresh_token)
-        print("cookies here", request.cookies)
         if not refresh_token:
             return None, {"status": "error", "message": "No refresh token found"}
         
         try:
             payload = self.decode_token(refresh_token)
-            print("refresh token payload", payload)
+            user_uuid = payload["user_uuid"]
+            is_root = get_user_root_info(user_uuid)
+
             new_access_token = self.create_access_token(
-                payload["user_uuid"], payload.get("is_root")
+                user_uuid, is_root
             )
             return new_access_token, None
         except jwt.ExpiredSignatureError:
