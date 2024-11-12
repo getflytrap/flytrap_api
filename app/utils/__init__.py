cfc23ab3a6e1557d5c0908fadc00e4d4dd9e2448
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-12 15:43:04 +0100

    feat: implement methods to interact with aws sdk
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -10,6 +10,7 @@ from .db_helpers import (
 )
 from .uuid_generator import generate_uuid
 from .validation import is_valid_email
+from .aws_helpers import create_aws_client, associate_api_key_with_usage_plan
 
 __all__ = [
     "db_read_connection",
@@ -20,4 +21,6 @@ __all__ = [
     "generate_uuid",
     "is_valid_email",
     "calculate_total_user_project_pages",
+    "create_aws_client", 
+    "associate_api_key_with_usage_plan"
 ]
