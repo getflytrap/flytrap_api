6efd3f93b9c8fbb44b2280ffa0041199670aa23f
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-14 22:51:47 +0100

    chore: rename webhook.py to notifications.py to hold all notification related functionality
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/routes/webhook.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify, Blueprint, Response
-
-bp = Blueprint("webhook", __name__)
-
-
-@bp.route("", methods=["POST"])
-def receive_webhook() -> Response:
-    """Receives a webhook POST request."""
-    return jsonify({"status": "success", "message": "Webhook received."}), 200
