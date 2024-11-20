0bd6d5557d4858f4448268622f452dc9dc72bcaa
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-20 10:34:55 +0100

    fix: correct retrieval of secrets to match the format they're stored in per terraform instructions
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/config.py
+++ app/config.py
@@ -1,13 +1,13 @@
 """Configuration module for loading environment variables."""
 
 import os
+import json
 import boto3
 from typing import Optional
 from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 
 def get_secret(secret_name, region_name):
-    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -23,6 +23,7 @@ def get_secret(secret_name, region_name):
 
     return get_secret_value_response['SecretString']
 
+ENVIRONMENT = os.getenv("FLASK_ENV", "development")
 USAGE_PLAN_ID = os.getenv("USAGE_PLAN_ID")
 AWS_REGION = os.getenv("AWS_REGION")
 DB_HOST: Optional[str] = os.getenv("PGHOST")
@@ -34,7 +35,6 @@ SECURE = True if os.getenv("SECURE") == "True" else False
 SAMESITE = os.getenv("SAMESITE")
 PATH = os.getenv("PATH")
 
-ENVIRONMENT = os.getenv("FLASK_ENV", "development")
 
 if ENVIRONMENT == "development":
   load_dotenv()
@@ -42,11 +42,11 @@ if ENVIRONMENT == "development":
   JWT_SECRET_KEY: Optional[str] = os.getenv("JWT_SECRET_KEY")
 else:
   try:
-    JWT_SECRET_KEY = get_secret('flytrap/jwt_secret_key', AWS_REGION)
-    DB_PASSWORD = get_secret('flytrap/pg_password', AWS_REGION)
-    print('successfully retrieved secrets')
+    jwt_secret = get_secret("jwt_secret_key", AWS_REGION)
+    JWT_SECRET_KEY = json.loads(jwt_secret)["jwt_secret_key"]
+
+    db_credentials = get_secret("flytrap_db_credentials", AWS_REGION)
+    DB_PASSWORD = json.loads(db_credentials)["password"]
   except Exception as e:
-    print(f"Error fetching JWT_SECRET_KEY from Secrets Manager: {e}")
     JWT_SECRET_KEY = None
     DB_PASSWORD = None
-    JWT_SECRET_KEY = None
