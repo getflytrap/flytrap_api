fbb57717eb9780e45522f88819cfc28329edf66c
Saul <saulthompson@users.noreply.github.com>
2024-12-04 03:16:53 +0000

    fix: Added mock model call for add_project
    Co-authored-by: Clarissa Röder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- app/routes/unit_tests/test_projects.py
+++ app/routes/unit_tests/test_projects.py
@@ -16,8 +16,9 @@ app.config["TESTING"] = True
 def client():
     return app.test_client()
 
+@patch('app.routes.projects.add_project')
 @patch('app.routes.projects.generate_uuid')
-def test_create_project(mock_generate_uuid, client):
+def test_create_project(mock_generate_uuid, mock_add_project, client):
     mock_generate_uuid.return_value = "dajhew32876dcx79sd2332"
 
     response = client.post('/api/projects', json={
