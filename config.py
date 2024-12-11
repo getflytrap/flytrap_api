efc29a8436de8a0db54108f640fa0a3208c49f93
Saul <saulthompson@users.noreply.github.com>
2024-12-11 12:14:29 +0000

    refactor: Reorganized the main flytrap config.py file.
    Co-authored-by: Clarissa Röder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- config.py
+++ config.py
@@ -12,6 +12,18 @@ class TestingConfig:
     DB_PASSWORD = "postgres"
     JWT_SECRET_KEY = '23ljdskj3jh23b432k'
 
+class StandardConfig:
+    """Development and Production configuration."""
+    DB_HOST = os.getenv("PGHOST")
+    DB_NAME = os.getenv("PGDATABASE")
+    DB_USER = os.getenv("PGUSER")
+    DB_PORT = os.getenv("PGPORT")
+    HTTPONLY = os.getenv("HTTPONLY") == "True"
+    SECURE = os.getenv("SECURE") == "True"
+    SAMESITE = os.getenv("SAMESITE")
+    DB_PASSWORD = os.getenv("PGPASSWORD")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
 
 def load_config(app):
     """
@@ -22,27 +34,30 @@ def load_config(app):
 
     if app.config["ENVIRONMENT"] == 'TESTING':
         app.config.from_object(TestingConfig)
-    elif app.config["ENVIRONMENT"] == "development":
-        from dotenv import load_dotenv
 
-        load_dotenv()
-
-        app.config["DB_PASSWORD"] = os.getenv("PGPASSWORD")
-        app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     else:
-        from app.utils import get_secret
+        if app.config["ENVIRONMENT"] == "development":
+            from dotenv import load_dotenv
 
-        try:
-            # Load secrets from AWS Secrets Manager
-            jwt_secret = get_secret("jwt_secret_key", app.config["AWS_REGION"])
-            app.config["JWT_SECRET_KEY"] = json.loads(jwt_secret)["jwt_secret_key"]
+            load_dotenv()
+        else:
+            """Set production configuration"""
+            from app.utils import get_secret
 
-            db_credentials = get_secret(
-                "flytrap_db_credentials", app.config["AWS_REGION"]
-            )
-            app.config["DB_PASSWORD"] = json.loads(db_credentials)["password"]
-        except Exception as e:
-            app.logger.error(f"Failed to load secrets: {e}")
-            app.config["DB_PASSWORD"] = None
-            app.config["JWT_SECRET_KEY"] = None
-            raise e
+            try:
+                # Load secrets from AWS Secrets Manager
+                jwt_secret = get_secret("jwt_secret_key", app.config["AWS_REGION"])
+                app.config["JWT_SECRET_KEY"] = json.loads(jwt_secret)["jwt_secret_key"]
+
+                db_credentials = get_secret(
+                    "flytrap_db_credentials", app.config["AWS_REGION"]
+                )
+                app.config["DB_PASSWORD"] = json.loads(db_credentials)["password"]
+            except Exception as e:
+                app.logger.error(f"Failed to load secrets: {e}")
+                app.config["DB_PASSWORD"] = None
+                app.config["JWT_SECRET_KEY"] = None
+                raise e
+        
+        # Load configuration settings that are shared between development and production
+        app.config.from_object(StandardConfig)
